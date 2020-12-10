e0644d3cfdbbd69ee335c459c0771e4d508bdcbf
reCup-CoOC <reCup-imt@outlook.fr>
2020-12-10 21:41:38 +0100

    Test negative number of cups

--- operations/depot.py
+++ operations/depot.py
@@ -10,7 +10,7 @@ cursor = conn.cursor()
 
 #IDENTIFICATION PAR BADGE DE L'UTILISATEUR
 
-id_user = input("Entrez votre identifiant d'utilisateur : ")
+id_user = input("Entrez votre identifiant d'utilisateur : ") #A REMPLACER PAR LA BADGEUSE
 
 #REQUETE DE RECUPERATION DES INFORMATIONS DE L'UTILISATEUR QUI A BADGE
 query  = ("SELECT * FROM Utilisateur WHERE ID_USERS=%s ")
@@ -19,22 +19,23 @@ cursor.execute(query, (id_user, ))
 rows = cursor.fetchall()
 for row in rows:
    #print('{0} : {1} - {2}'.format(row[0], row[1], row[2]))
-   nb_tasses_empruntees = row[5] #Recuperation du nombre de tasses
-   solde = row[6]  #Recuperation du solde
+   nb_tasses_empruntees = row[6] #Recuperation du nombre de tasses
+   solde = row[7]  #Recuperation du solde
 
+if(nb_tasses_empruntees>0):
+  new_nb_tasses_empruntees = nb_tasses_empruntees - 1 #Calcul du nombre total de tasses empruntees
+  caution = 2 #Caution de 2€ rendue suite au depot
 
-new_nb_tasses_empruntees = nb_tasses_empruntees - 1 #Calcul du nombre total de tasses empruntees
-caution = 2 #Caution de 2€ rendue suite au depot
+  #MODIFICATION DU NOMBRE DE TASSES EMPRUNTEES ET DU SOLDE DE L'UTILISATEUR
+  solde = solde + caution
+  solde = str(solde)
+  new_nb_tasses_empruntees = str(new_nb_tasses_empruntees)
+  sql = "UPDATE Utilisateur SET nb_tasses_empruntees = %s, solde=%s WHERE ID_USERS =%s"
 
-#MODIFICATION DU NOMBRE DE TASSES EMPRUNTEES ET DU SOLDE DE L'UTILISATEUR
-solde = solde + caution
-solde = str(solde)
-new_nb_tasses_empruntees = str(new_nb_tasses_empruntees)
-sql = "UPDATE Utilisateur SET nb_tasses_empruntees = %s, solde=%s WHERE ID_USERS =%s"
+  cursor.execute(sql, (new_nb_tasses_empruntees, solde, id_user)) #Cree une requete pour decrementer le nombre total de tasses empruntees  
 
-cursor.execute(sql, (new_nb_tasses_empruntees, solde, id_user)) #Cree une requete pour decrementer le nombre total de tasses empruntees  
+  conn.commit() #Envoie la requête à la base de données
 
-conn.commit() #Envoie la requête à la base de données
 
 conn.close()
 
